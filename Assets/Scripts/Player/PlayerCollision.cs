24dff6b69ced55dc67979413f1aeadc0c0884806
Joel Brubaker <joel@rescuescg.com>
2015-06-18 09:05:20 -0700

    Started adding slow digestion of food

--- Assets/Scripts/Player/PlayerCollision.cs
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -3,30 +3,44 @@ using System.Collections;
 
 public class PlayerCollision : MonoBehaviour {
 
+	private GameObject collisionObject;
 
+	private float playerEnergy;
+	private float foodEnergy;
+	private float playerDrag;
 
-	void Start () {
-
-	}
-
-	void OnCollisionEnter2D (Collision2D collision) {
-
+	void OnTriggerEnter2D(Collider2D collider) {
+		Debug.Log ("Trigger Enter");
 		// get collision object
-		GameObject collisionObject = collision.gameObject;
+		collisionObject = collider.gameObject;
 
 		// check if object is food
 		if (collisionObject.name == "Food(Clone)") {
-			GameObject food = collisionObject;
-
-			float foodEnergy = food.GetComponent<FoodAttributes>().energy;
-			float playerEnergy = GetComponent<PlayerAttributes>().energy;
-
-			// add food energy to player
-			GetComponent<PlayerAttributes>().energy = playerEnergy + foodEnergy;
-
-			Destroy(food);
-
+			playerDrag = GetComponent<Rigidbody2D> ().drag;
+			Debug.Log(playerDrag);
+			GetComponent<Rigidbody2D> ().drag = 10.0f;
+			foodEnergy = collisionObject.GetComponent<FoodAttributes> ().energy;
+			playerEnergy = GetComponent<PlayerAttributes> ().energy;
 		}
+	}
+
+	void OnTriggerStay2D(Collider2D collider) {
+		float transferEnergy = foodEnergy * Time.deltaTime / 4.0f;
+		GetComponent<PlayerAttributes> ().energy += transferEnergy;
+		collisionObject.GetComponent<FoodAttributes> ().energy += -transferEnergy;
+		if (collisionObject.GetComponent<FoodAttributes> ().energy <= 0) {
+			Destroy(collisionObject);
+		}
+	}
+
+	void OnTriggerExit2D() {
+		Debug.Log ("Trigger Exit");
+		Debug.Log(playerDrag);
+		GameObject collisionObject = null;
+		foodEnergy = 0f;
+		playerEnergy = 0f;
+		GetComponent<Rigidbody2D> ().drag = playerDrag;
+		playerDrag = 0f;
 	}
 	
 }
--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -12,8 +12,6 @@ public class PlayerMove : MonoBehaviour {
 	void FixedUpdate() {
 		float y = Input.GetAxis("Vertical") * speed;
 		float x = Input.GetAxis("Horizontal") * speed;
-		y *= Time.deltaTime;
-		x *= Time.deltaTime;
 		Rigidbody2D playerBody = GetComponent<Rigidbody2D>();
 		playerBody.AddForce(new Vector2(x,y));
 	}
